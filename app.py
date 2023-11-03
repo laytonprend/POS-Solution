53be5751f0befc1478b26d316061fd02c3edf34a
laytonprend <128324986+laytonprenduni@users.noreply.github.com>
2023-11-03 17:04:44 +0000

    issue with main buttons

--- app.py
+++ app.py
@@ -8,30 +8,30 @@ import streamlit as st
 import pandas as pd
 from datetime import datetime
 import shutil
+import numpy as np
 
 # Load the product and price data from Excel files
 @st.cache_data
 def load_data(file_path):
     data = pd.read_excel(file_path)
-    return data
-
-product_data = load_data("products.xlsx").copy()
-price_data = load_data("price.xlsx").copy()
-
-# Filter price data
-date_format = "%d/%m/%Y"
-price_data['Date'] = pd.to_datetime(price_data['Date'], format=date_format, errors='coerce')
-price_data = price_data.sort_values(by=['Product ID', 'Date'], ascending=[True, False])
-
-# Keep only the most recent entry for each product
-price_data = price_data.drop_duplicates(subset='Product ID', keep='first')
-
-# Function to update price data
-def update_price_data(new_price, product_id):
-    new_price_entry = {"Product ID": product_id, "Price": new_price, "Date": datetime.now()}
+    return data.copy()
+def load_price():
     price_data = load_data("price.xlsx")
-    price_data = price_data.append(new_price_entry, ignore_index=True)
-    price_data.to_excel("price.xlsx", index=False)
+    # Filter price data
+    date_format = "%d/%m/%Y"
+    price_data['Date'] = pd.to_datetime(price_data['Date'], format=date_format, errors='coerce')
+    price_data = price_data.sort_values(by=['Product_ID', 'Date'], ascending=[True, False])
+    
+    # Keep only the most recent entry for each product
+    price_data = price_data.drop_duplicates(subset='Product_ID', keep='first')
+    return price_data
+product_data = load_data("products.xlsx")
+
+price_data=load_price()
+
+
+#product_data=product_data.merge(price_data,by='Product_ID',how='left')
+
 
 # Streamlit app
 st.title("Restaurant POS System")
@@ -40,7 +40,8 @@ st.title("Restaurant POS System")
 st.header("Shopping Cart")
 
 cart = st.session_state.cart if "cart" in st.session_state else {
-    "products": [],
+    "product_id": [],
+    "product_name": [],
     "price": [],
     "quantity": []
 }
@@ -51,26 +52,33 @@ button_columns = st.columns(buttons_per_row)
 #def callback():#buttonw as clicked
  #   st.session_state.button_clicked=True
     #cart
-# Loop through the product IDs to create buttons
-for product_id in product_data['Product ID']:
-    if button_columns[product_id % buttons_per_row].button(f"Add to Cart (Product ID {product_id})", key=f"button_add_{product_id}"):
-        product_index = cart["products"].index(product_id) if product_id in cart["products"] else -1
+# Loop through the Product_IDs to create buttons
+for product_id, product_name in zip(product_data['Product_ID'], product_data['Product_Name']):
+    product_id = int(product_id)  # Convert product_id to an integer
+    if button_columns[product_id % buttons_per_row].button(f"Add to Cart Product {product_id}, {product_name}", key=f"button_add_{product_id}, {product_name}"):
+        product_index = cart["product_id"].index(product_id) if product_id in cart["product_id"] else -1
 
         if product_index != -1:
             # Update quantity and price for existing product
             cart["quantity"][product_index] += 1
         else:
             # Add new product to cart
-            cart["products"].append(product_id)
-            cart["price"].append(price_data.loc[price_data['Product ID'] == product_id, 'Price'].values[0])
-            cart["quantity"].append(1)
+            
+            try:
+                cart["price"].append(price_data.loc[price_data['Product_ID'] == product_id, 'Price'].values[-1]) # price first as may not be in
+                cart["product_id"].append(product_id)
+                cart["product_name"].append(product_data.loc[product_data['Product_ID'] == product_id, 'Product_Name'].values[-1])
+                cart["quantity"].append(1)
+            except IndexError:
+                st.title(f'Please add price data for {product_name}, unable to add to cart')
+            
             
         st.session_state.cart=cart # key to write back to session state
 
 # Display items in the cart
 remove_indices = []
-for i, product_id in enumerate(cart["products"]):
-    if st.button(f"Remove from Cart (Product ID {product_id})", key=f"button_remove_{product_id}"):
+for i, product_id in enumerate(cart["product_id"]):
+    if st.button(f"Remove from Cart (Product_ID {product_id})", key=f"button_remove_{product_id}"):
         if cart["quantity"][i] > 1:
             cart["quantity"][i] -= 1
         else:
@@ -78,27 +86,85 @@ for i, product_id in enumerate(cart["products"]):
 
 # Remove items marked for removal
 for i in reversed(remove_indices):
-    del cart["products"][i]
+    del cart["product_id"][i]
+    del cart["product_name"][i]
     del cart["price"][i]
     del cart["quantity"][i]
+    st.session_state.cart=cart # key to write back to session state
 
 # Calculate total price
 total_price = sum([price * quantity for price, quantity in zip(cart["price"], cart["quantity"])])
 
 # Display the cart contents
 st.write("Cart Items:")
-for product_id, price, quantity in zip(cart["products"], cart["price"], cart["quantity"]):
-    st.write(f"Product ID: {product_id}, Price: ${price:.2f}, Quantity: {quantity}")
+for product_id, product_name, price, quantity in zip(cart["product_id"],cart["product_name"], cart["price"], cart["quantity"]):
+    st.write(f"Product_ID: {product_id}, Product_Name: {product_name}, Price: ${price:.2f}, Quantity: {quantity}")
 st.write(f"Total Price: ${total_price:.2f}")
 
-# Developer Options
-st.sidebar.header("Developer Options")
+if st.button("Checkout"):
+    transactions_prev = load_data("transactions.xlsx")
+    transaction_id=np.max(transactions_prev['Transaction_ID'])+1#new transaction ID
+    transactions = []
+    for product_id, product_name, product_price, product_quantity in zip(cart["product_id"], cart["product_name"], cart["price"], cart["quantity"]):# in cart.items():
+        #item_price=product_data.loc[]
+        transactions.append({"Transaction_ID":transaction_id, "Date": datetime.now(), "Total_Price":product_price*product_quantity, 'Unit_Price':product_price ,"Quantity":product_quantity ,
+                             "Product_ID": product_id, "Product_Name": product_name})
 
-if st.sidebar.button("Update Price Data"):
-    new_price = st.sidebar.number_input("New Price:", step=0.01)
-    product_id = st.sidebar.number_input("Product ID:")
-    update_price_data(new_price, product_id)
-    st.sidebar.success("Price data updated.")
+        
+        #for i in range(product_info["quantity"]):
+         #   transactions.append({"Product_ID": product_id, "Price": product_info["price"], "Date": datetime.now()})
+    #transactions = load_data("transactions.xlsx")
+    transactions_df = pd.concat([transactions_prev, pd.DataFrame(transactions)])
+    transactions_df.to_excel("transactions.xlsx", index=False)
+    st.success("Checkout successful. Transaction data saved to 'transactions.xlsx'")
+
+# Developer Options
+def update_product_data(new_product_id,new_product_name):
+    new_product_entry = {"Product_ID": [product_id], "Product_Name": [new_product_name], "Date": [datetime.now()]}
+    new_product_df = pd.DataFrame(new_product_entry)
+    # Load the existing price data
+    product_data = load_data("price.xlsx")
+    # Append the new data to the existing data
+    updated_product_data = pd.concat([product_data, new_product_df], ignore_index=True)
+    # Save the updated price data to the Excel file
+    updated_product_data.to_excel("products.xlsx", index=False)
+st.sidebar.header("Developer Options")
+##product update
+UpdateProduct = st.session_state.UpdateProduct if "UpdateProduct" in st.session_state else False
+ProductSubmission = st.session_state.ProductSubmission if "ProductSubmission" in st.session_state else []
+if st.sidebar.button("Update Product Data") or UpdateProduct:
+    st.session_state.UpdateProduct=True
+    new_product_id = np.max(product_data['Product_ID'])+1#st.sidebar.number_input("Product_ID:",step=1)
+    new_product_name = st.sidebar.text_input("New Product Name:")
+    if st.sidebar.button("Submit New Product") and ProductSubmission!=[new_product_id,new_product_name]:# maybe block duplicates too
+        st.session_state.ProductSubmission=[new_product_id,new_product_name]
+        
+        update_product_data(new_product_id,new_product_name)
+        st.sidebar.success("Product data updated.")
+        product_data = load_data("products.xlsx")
+# Function to update price data
+def update_price_data(new_price, product_id):
+    new_price_entry = {"Product_ID": [product_id], "Price": [new_price], "Date": [datetime.now()]}
+    new_price_df = pd.DataFrame(new_price_entry)
+    # Load the existing price data
+    price_data = load_data("price.xlsx")
+    # Append the new data to the existing data
+    updated_price_data = pd.concat([price_data, new_price_df], ignore_index=True)
+    # Save the updated price data to the Excel file
+    updated_price_data.to_excel("price.xlsx", index=False)
+##price update
+UpdatePrice = st.session_state.UpdatePrice if "UpdatePrice" in st.session_state else False
+PriceSubmission = st.session_state.PriceSubmission if "PriceSubmission" in st.session_state else []
+if st.sidebar.button("Update Price Data") or UpdatePrice:
+    st.session_state.UpdatePrice=True
+    product_id = st.sidebar.number_input("Product_ID:",step=1)
+    new_price = st.sidebar.number_input("New Price:", step=0.25)
+    if st.sidebar.button("Submit New Price") and PriceSubmission!=[product_id,new_price]:# maybe block duplicates too
+        st.session_state.PriceSubmission=[product_id,new_price]
+        
+        update_price_data(new_price, product_id)
+        st.sidebar.success("Price data updated.")
+        price_data=load_price()
 
 if st.sidebar.button("Clear Cart"):
     cart = {
@@ -107,7 +173,7 @@ if st.sidebar.button("Clear Cart"):
         "quantity": []
     }
 
-if st.sidebar.button("Backup Data"):
+if st.sidebar.button("Backup Data"): # shutil is a convenient and powerful module for working with files and directories in Python, making various file-related tasks easier to perform.
     backup_folder = "backup_data"
     shutil.rmtree(backup_folder, ignore_errors=True)
     shutil.copy("products.xlsx", backup_folder)
@@ -130,7 +196,7 @@ if st.sidebar.button("Show Price Data"):
     st.sidebar.dataframe(price_data)
 
 # Show Cart in Sidebar
-st.sidebar.header("Shopping Cart")
-for product_id, price, quantity in zip(cart["products"], cart["price"], cart["quantity"]):
-    st.sidebar.write(f"Product ID: {product_id}, Price: ${price:.2f}, Quantity: {quantity}")
-
+'''st.sidebar.header("Shopping Cart")
+for product_id, product_name, price, quantity in zip(cart["product_id"], cart["product_name"], cart["price"], cart["quantity"]):
+    st.sidebar.write(f"Product_ID: {product_id}, Product_Name: {product_name}, Price: ${price:.2f}, Quantity: {quantity}")
+'''
